a67e68d1af90bd17db9b529f73f34f580eead9a1
Archbaer <pereirakebaili@gmail.com>
2025-01-27 17:36:34 +0000

    only views left to be done

--- littlelemon/LittleLemonAPI/models.py
+++ littlelemon/LittleLemonAPI/models.py
@@ -5,11 +5,17 @@ class Category(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=255, db_index=True)
 
+    def __str__(self):
+        return self.title
+
 class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
     featured = models.BooleanField(db_index=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -21,12 +27,18 @@ class Cart(models.Model):
     class Meta:
         unique_together = ('menuitem', 'user')
 
+    def __str__(self):
+        return self.user.username
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     delivery_crew = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True)
     status = models.BooleanField(db_index=True, default=0)
     total = models.DecimalField(max_digits=6, decimal_places=2)
     date = models.DecimalField(db_index=True)
+
+    def __str__(self):
+        return self.user.username
 
 class OrderItem(models.Model):
     order = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -37,4 +49,7 @@ class OrderItem(models.Model):
 
     class Meta:
         unique_together = ('order', 'menuitem')
+
+    def __str__(self):
+        return self.order.user.username
 
---
+++ littlelemon/LittleLemonAPI/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urls = [
+    path('', include("djoser.urls")),
+    path('menu-items', views.MenuItemView, name='menu-items'),
+    path('menu-items/<int:pk>', views.SingleMenuItemView, name='single-menu-item'),
+
+    path('groups/manager/users', views.ManagerView, name="managers"),
+    path('groups/manager/users/<str:username>', views.DeleteManagerView, name="delete-manager"),
+    
+    path('groups/delivery-crew/users', views.DeliveryCrewView, name="delivery-crews"),
+    path('groups/delivery-crew/users/<str:username>', views.DeleteDeliveryCrewView, name="delete-delivery-crew"),
+
+    path('cart/menu-items', views.CartView, name="cartview"),
+
+    path("orders", views.OrderView, name="orders"),
+    path("orders/<int:pk>", views.SingleOrderView, name="single-order"),
+]
